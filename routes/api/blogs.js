a70c6572d7fa4d2a60a7bc047f8cf6a3adc2edca
JaiRaga <raga.j.santhosh@gmail.com>
2020-07-20 11:06:35 +0530

    created api to retrieve blog by id

--- routes/api/blogs.js
+++ routes/api/blogs.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Blog = require("../../models/Blog");
 const router = express.Router();
 
+// Get blogs from all categories
 router.get("/blogs", async (req, res) => {
   try {
     const blogs = await Blog.find();
@@ -12,12 +13,34 @@ router.get("/blogs", async (req, res) => {
   }
 });
 
+// Create a blog
 router.post("/blogs", async (req, res) => {
-  const blog = new Blog({ ...req.body });
+  let { title, body, category } = req.body;
+  let readtime = Math.round(body.length / 4);
+
+  category = category.replace(" ", "").split(",");
+
+  const blog = new Blog({ title, body, category, readtime });
 
   try {
     await blog.save();
     res.status(201).send(blog);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
+// Get blog by id
+router.get("/blog/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const blog = await Blog.findById(id);
+    console.log(blog);
+
+    if (!blog) return res.status(404).send("No such blog exists!");
+
+    res.send(blog);
   } catch (err) {
     res.status(500).send("Server Error");
   }
