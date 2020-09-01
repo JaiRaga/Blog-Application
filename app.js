3d68b97430a765cb8c712ae3935571ef3167eea5
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-01 11:48:23 +0530

    created user router for accessing user routes

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const express = require("express");
 const app = express();
 
 require("./mongoose/mongoose");
+const usersRouter = require("./routes/api/users");
 const blogsRouter = require("./routes/api/blogs");
 
 app.use(express.json());
 
 const port = process.env.PORT || 9008;
 
+app.use("/api", usersRouter);
 app.use("/api", blogsRouter);
+
 app.listen(port, console.log(`Running on Port ${port}`));
