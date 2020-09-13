f76c2cb9bf6b047e699064e466b9b88e6821eaa5
JaiRaga <raga.j.santhosh@gmail.com>
2020-09-13 15:50:13 +0530

    created delete endpoint for blog

--- routes/api/blogs.js
+++ routes/api/blogs.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const Blog = require("../../models/Blog");
+const auth = require("../../middleware/auth");
 const router = express.Router();
 
 // Create a blog
-router.post("/blogs", async (req, res) => {
+router.post("/blogs", auth, async (req, res) => {
   let { title, body, category, stars } = req.body;
+  const owner = req.user._id;
   // let readtime = Math.round(body.length / 200);
   if (!stars) stars = 4;
 
   category = category.toLowerCase().replace(" ", "").split(",");
 
-  const blog = new Blog({ title, body, category, stars });
+  const blog = new Blog({ title, body, category, stars, owner });
 
   try {
     await blog.save();
@@ -21,7 +23,7 @@ router.post("/blogs", async (req, res) => {
 });
 
 // Get blogs from all categories
-router.get("/blogs", async (req, res) => {
+router.get("/blogs", auth, async (req, res) => {
   let category = [],
     sort = {},
     limit = parseInt(req.query.limit),
@@ -58,7 +60,7 @@ router.get("/blogs", async (req, res) => {
 });
 
 // Get blog by id
-router.get("/blog/:id", async (req, res) => {
+router.get("/blog/:id", auth, async (req, res) => {
   const id = req.params.id;
 
   try {
@@ -74,7 +76,7 @@ router.get("/blog/:id", async (req, res) => {
 });
 
 // Update blog
-router.patch("/blog/:id", async (req, res) => {
+router.patch("/blog/:id", auth, async (req, res) => {
   const incomingUpdates = Object.keys(req.body),
     allowedUpdates = ["title", "body", "category", "stars"];
 
@@ -92,7 +94,7 @@ router.patch("/blog/:id", async (req, res) => {
 
   try {
     const blog = await Blog.findById(req.params.id);
-    if (!blog) return res.send("No Such Blog Exists!");
+    if (!blog) return res.status(404).send("No Such Blog Exists!");
 
     req.body.category = req.body.category
       .toLowerCase()
@@ -101,6 +103,22 @@ router.patch("/blog/:id", async (req, res) => {
 
     approvedUpdates.forEach((update) => (blog[update] = req.body[update]));
     await blog.save();
+
+    res.send(blog);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+});
+
+// Delete a Blog
+router.delete("/blog/:id", auth, async (req, res) => {
+  try {
+    const blog = await Blog.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id
+    });
+
+    if (!blog) return res.status(404).send("No Blog Found!");
 
     res.send(blog);
   } catch (err) {
